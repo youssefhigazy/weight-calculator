eed4c55a34d163a324e0207ab484b0c8481a1fed
Youssef Higazy <yoyohigazy77@gmail.com>
2021-04-09 02:53:04 -0500

    Created a template for the row-measurement

--- src/app/measurement/measurement.component.ts
+++ src/app/measurement/measurement.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./measurement.component.scss']
 })
 export class MeasurementComponent implements OnInit {
-
+  measurements: any[];
+  weights: any[];
   constructor() { }
 
   ngOnInit(): void {
+    this.measurements = JSON.parse(localStorage.getItem("current-measurement-information"));
+    this.weights = JSON.parse(localStorage.getItem("weight-data"));
   }
 
 }
