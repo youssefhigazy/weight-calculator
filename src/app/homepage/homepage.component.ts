93fe535977f9a5d2f59220c1856bc07672b3a2a6
Youssef Higazy <yoyohigazy77@gmail.com>
2021-07-06 01:15:09 -0500

    Updating the home page's title

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-homepage',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Home page");
+  }
 
   ngOnInit(): void {
   }
