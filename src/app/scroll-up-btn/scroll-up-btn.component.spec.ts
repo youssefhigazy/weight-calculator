84f754d7e105922f14b715131dd664992d3954c1
Youssef Higazy <yoyohigazy77@gmail.com>
2021-07-06 01:12:23 -0500

    Creating the scroll-up-btn component

---
+++ src/app/scroll-up-btn/scroll-up-btn.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScrollUpBtnComponent } from './scroll-up-btn.component';
+
+describe('ScrollUpBtnComponent', () => {
+  let component: ScrollUpBtnComponent;
+  let fixture: ComponentFixture<ScrollUpBtnComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ScrollUpBtnComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ScrollUpBtnComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
